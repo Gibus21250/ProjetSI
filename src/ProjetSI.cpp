029f0cff8d6083c361c3df0644f3c6b37f5c0d6e
Gibus21250 <jacquinet.nathan@gmail.com>
2022-11-03 19:19:43 +0100

    first commit

---
+++ src/ProjetSI.cpp
@@ -0,0 +1,11 @@
+﻿// ProjetSI.cpp : Defines the entry point for the application.
+
+#include "../include/ProjetSI.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
